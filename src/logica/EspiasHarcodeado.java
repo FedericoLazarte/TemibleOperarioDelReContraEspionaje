8069aaf68b1632af74346cace90e3f0f35eea5f2
FedericoLazarte <fedelazarte2015@gmail.com>
2024-09-27 19:09:25 -0300

    añadiendo nombres a los espías y hardcodeando

--- src/logica/EspiasHarcodeado.java
+++ src/logica/EspiasHarcodeado.java
@@ -4,31 +4,30 @@ import org.openstreetmap.gui.jmapviewer.Coordinate;
 
 public class EspiasHarcodeado {
 	
-	private static final Espia espia1 = new Espia("Buenos Aires", new Coordinate(-36.5, -60.5));
-	// En el enunciado se incluye espia2. Para fines prácticos, se la considera Espia
-	private static final Espia espia2 = new Espia("Ciudad Autónoma de Buenos Aires", new Coordinate(-34.6037, -58.3816));
-	private static final Espia espia3 = new Espia("espia3", new Coordinate(-27.25, -67.25));
-	private static final Espia espia4 = new Espia("espia4", new Coordinate(-26.5, -60.75));
-	private static final Espia espia5 = new Espia("espia5", new Coordinate(-44, -68.5));
-	private static final Espia espia6 = new Espia("Córdoba", new Coordinate(-32, -63.8));
-	private static final Espia espia7 = new Espia("espia7", new Coordinate(-28.75, -57.9));
-	private static final Espia espia8 = new Espia("Entre Ríos", new Coordinate(-32, -59.35));
-	private static final Espia espia9 = new Espia("espia9", new Coordinate(-24.75, -60));
-	private static final Espia espia10 = new Espia("espia10", new Coordinate(-23, -66));
-	private static final Espia espia11 = new Espia("La Rioja", new Coordinate(-29.4131, -67.25));
-	private static final Espia espia12 = new Espia("La Pampa", new Coordinate(-37.25, -65.5));
-	private static final Espia espia13 = new Espia("espia13", new Coordinate(-34.5, -68.5));
-	private static final Espia espia14 = new Espia("espia14", new Coordinate(-27, -54.75));
-	private static final Espia espia15 = new Espia("Neuquén", new Coordinate(-38.5, -70));
-	private static final Espia espia16 = new Espia("Río Negro", new Coordinate(-40.25, -67.25));
-	private static final Espia espia17 = new Espia("espia17", new Coordinate(-25.25, -64.75));
-	private static final Espia espia18 = new Espia("San Juan", new Coordinate(-30.75, -68.8));
-	private static final Espia espia19 = new Espia("San Luis", new Coordinate(-33.75, -66.1));
-	private static final Espia espia20 = new Espia("Santa Cruz", new Coordinate(-48.6226, -70));
-	private static final Espia espia21 = new Espia("Santa Fe", new Coordinate(-30.6107, -61));
-	private static final Espia espia22 = new Espia("Santiago del Estero", new Coordinate(-27.7951, -63.5));
-	private static final Espia espia23 = new Espia("Tierra del Fuego", new Coordinate(-54.25, -67.7));
-	private static final Espia espia24 = new Espia("Tucumán", new Coordinate(-26.9, -65.4));
+	private static final Espia espia1 = new Espia("Abigail Brand", new Coordinate(-36.5, -60.5));
+	private static final Espia espia2 = new Espia("Allan Thompson", new Coordinate(-34.6037, -58.3816));
+	private static final Espia espia3 = new Espia("Henry Peter Gyrich", new Coordinate(-27.25, -67.25));
+	private static final Espia espia4 = new Espia("Tom Quinn", new Coordinate(-26.5, -60.75));
+	private static final Espia espia5 = new Espia("Connie James", new Coordinate(-44, -68.5));
+	private static final Espia espia6 = new Espia("Julia Carpenter", new Coordinate(-32, -63.8));
+	private static final Espia espia7 = new Espia("Michael Scofield", new Coordinate(-28.75, -57.9));
+	private static final Espia espia8 = new Espia("Adam Carter", new Coordinate(-32, -59.35));
+	private static final Espia espia9 = new Espia("Chuck Bartowski", new Coordinate(-24.75, -60));
+	private static final Espia espia10 = new Espia("Sarutobi Sasuke", new Coordinate(-23, -66));
+	private static final Espia espia11 = new Espia("Renée Rienne", new Coordinate(-29.4131, -67.25));
+	private static final Espia espia12 = new Espia("Beth Bailey", new Coordinate(-37.25, -65.5));
+	private static final Espia espia13 = new Espia("Harry Pearce", new Coordinate(-34.5, -68.5));
+	private static final Espia espia14 = new Espia("Hiro Takachiho", new Coordinate(-27, -54.75));
+	private static final Espia espia15 = new Espia("Araña Escarlata", new Coordinate(-38.5, -70));
+	private static final Espia espia16 = new Espia("Philip Jennings", new Coordinate(-40.25, -67.25));
+	private static final Espia espia17 = new Espia("Tessa Phillips", new Coordinate(-25.25, -64.75));
+	private static final Espia espia18 = new Espia("Odd Squad", new Coordinate(-30.75, -68.8));
+	private static final Espia espia19 = new Espia("Agent Zero", new Coordinate(-33.75, -66.1));
+	private static final Espia espia20 = new Espia("Lucas North", new Coordinate(-48.6226, -70));
+	private static final Espia espia21 = new Espia("Goku", new Coordinate(-30.6107, -61));
+	private static final Espia espia22 = new Espia("Renée Rienne", new Coordinate(-27.7951, -63.5));
+	private static final Espia espia23 = new Espia("Inspector Ardilla", new Coordinate(-54.25, -67.7));
+	private static final Espia espia24 = new Espia("Ricardo Fort", new Coordinate(-26.9, -65.4));
 
 	private static boolean seAgregaronEspiasCercanos = false;
 
